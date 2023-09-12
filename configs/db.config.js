d2be2f9fcca63aab2935418d7a330bdf33e9be5e
Aditya singh <akash.aditya001@gmail.com>
2023-09-12 05:40:37 +0530

    update

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = { 
     DB_NAME: "crm_db",
-    DB_URL: `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}akash123@cluster0.n3zwp8b.mongodb.net/crm_db`
+    DB_URL: `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.n3zwp8b.mongodb.net/crm_db`
 }
